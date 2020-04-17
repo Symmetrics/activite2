2c05c1a198bda9f3de3656a3e6c8f4414d6a09ec
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-04-17 14:59:23 +0000

    Providing IDE hints for mocks

--- lesson1_apriori.php
+++ lesson1_apriori.php
@@ -2,3 +2,8 @@
 <?php
 
 require_once('library/Apriori/lib/Apriori.class.php');
+
+//variables
+$minSupp  = 5;                  //minimal support
+$minConf  = 75;                 //minimal confidence
+$type     = Apriori::SRC_PLAIN; //data type
