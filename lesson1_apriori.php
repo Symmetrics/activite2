3761ade0b1953b09de90cc374aa9d22cd07706d4
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-04-18 18:17:35 +0000

    locks: fix unlock when fcntl_setlk races with a close

--- lesson1_apriori.php
+++ lesson1_apriori.php
@@ -7,3 +7,6 @@ require_once('library/Apriori/lib/Apriori.class.php');
 $minSupp  = 5;                  //minimal support
 $minConf  = 75;                 //minimal confidence
 $type     = Apriori::SRC_PLAIN; //data type
+$recomFor = 'sugar,beer';             //recommendation for
+
+$data = 'lesson1_dataset.txt';
