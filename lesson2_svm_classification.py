adad4cf4ecf1a504367dcb7e21a145eb9b41acc0
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-05-31 18:23:18 +0000

    update test cases

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -4,4 +4,4 @@ import os
 import FukuML.Utility as utility
 import FukuML.SupportVectorMachine as svm
 
-input_train_data_file = os.path.join(os.path.join(os
+input_train_data_file = os.path.join(os.path.join(os.ge
