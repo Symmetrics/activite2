f2bb8efd044aa235f210ad5bea310b8b16c377e0
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-06-01 08:25:17 +0000

    ipv6: don't accept multicast traffic with scope 0

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -4,4 +4,4 @@ import os
 import FukuML.Utility as utility
 import FukuML.SupportVectorMachine as svm
 
-input_train_data_file = os.path.join(os.path.join(os.ge
+input_train_data_file = os.path.join(os.path.join(os.getc
