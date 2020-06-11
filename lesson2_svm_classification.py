b3a1ba5d6c89193609958963ca42e48d3ee1f212
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-06-11 12:58:50 +0000

    Remove extra whitespace.

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -4,4 +4,4 @@ import os
 import FukuML.Utility as utility
 import FukuML.SupportVectorMachine as svm
 
-input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'libr
+input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/
