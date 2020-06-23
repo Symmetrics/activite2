e3a374e4caeec37922f7c5cd8c78a89aa6f422e4
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-06-23 22:14:35 +0000

    OAK-593: Segment-based MK

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -4,4 +4,4 @@ import os
 import FukuML.Utility as utility
 import FukuML.SupportVectorMachine as svm
 
-input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_com
+input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_combine_
