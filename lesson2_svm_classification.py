60aa607ddec39857f17ef3288697e7c5ed711b2c
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-06-24 19:06:18 +0000

    doc [ci skip]

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -4,4 +4,4 @@ import os
 import FukuML.Utility as utility
 import FukuML.SupportVectorMachine as svm
 
-input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_combine_song_
+input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_combine_song_trai
