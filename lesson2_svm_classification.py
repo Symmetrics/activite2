eae0d28dffc7e3f37fbb3f0ca7d0a095e7c92094
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-06-27 19:32:37 +0000

    AC

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -6,4 +6,4 @@ import FukuML.SupportVectorMachine as svm
 
 input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_combine_song_train.dataset')
 
-cross_validato
+cross_validator = u
