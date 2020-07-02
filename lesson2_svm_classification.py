a1dba6d553a1209cc52828fbf3d70ddb408855f8
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-07-02 07:24:33 +0000

    Fixed potential memory leak

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -6,4 +6,6 @@ import FukuML.SupportVectorMachine as svm
 
 input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/emotion_combine_song_train.dataset')
 
-cross_validator = utility.CrossValidator(
+cross_validator = utility.CrossValidator()
+
+sv
