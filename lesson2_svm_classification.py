42248e89c326a9ab32038785859dbde315bb8e45
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2020-07-11 06:20:03 +0000

    Delete wsgi.pyc

--- lesson2_svm_classification.py
+++ lesson2_svm_classification.py
@@ -8,4 +8,5 @@ input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(_
 
 cross_validator = utility.CrossValidator()
 
-svm_mc1 = svm.MultiClassifier
+svm_mc1 = svm.MultiClassifier()
+s
