08493ca626be3bca5ddc9815e44bb824d3dfeea5
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2023-03-01 22:29:29 +0000

    more instructions

--- lesson3_ridge_regression.py
+++ lesson3_ridge_regression.py
@@ -8,3 +8,7 @@ import FukuML.RidgeRegression as ridge_regression
 input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/valence_train.dataset')
 
 cross_validator = utility.CrossValidator()
+
+ridge_regression1 = ridge_regression.RidgeRegression()
+ridge_regression1.load_train_data(input_train_data_file)
+ridge_regression1.set_feature_transform('legendre', 2)
