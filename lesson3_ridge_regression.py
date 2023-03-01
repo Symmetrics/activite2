159cfd17624d79b5c434a3584f4e763f3c8eb3da
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2023-03-01 16:41:11 +0000

    Update libbuildpack

--- lesson3_ridge_regression.py
+++ lesson3_ridge_regression.py
@@ -6,3 +6,5 @@ import FukuML.Utility as utility
 import FukuML.RidgeRegression as ridge_regression
 
 input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/valence_train.dataset')
+
+cross_validator = utility.CrossValidator()
