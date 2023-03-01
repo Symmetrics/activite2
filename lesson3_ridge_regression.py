f6ce82754bb42d3946e7982a730582cba9f11eba
Symmetrics <106295188+Symmetrics@users.noreply.github.com>
2023-03-01 10:19:20 +0000

    Finish implementation

--- lesson3_ridge_regression.py
+++ lesson3_ridge_regression.py
@@ -1,2 +1,8 @@
 
 #encoding=utf8
+
+import os
+import FukuML.Utility as utility
+import FukuML.RidgeRegression as ridge_regression
+
+input_train_data_file = os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), 'library/iNDIEVOX-Dataset/dataset/valence_train.dataset')
